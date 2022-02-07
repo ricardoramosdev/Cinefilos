c37c970886416b5c03f3614ce446d63219f12945
gerogarzon <gerogarzon@gmail.com>
2022-02-07 10:38:49 -0300

    comiteo para mergear con lo de benja

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -3,6 +3,7 @@ function detallePelicula(index){
     
     window.location = "/pages/pagesinfo.html"
     let detalle = movies[index];
+    console.log("sai:", detalle);
     localStorage.setItem("detalle", JSON.stringify(detalle));
     console.log("acaaa:", detalle);
 
--- js/pagesInfo.js
+++ js/pagesInfo.js
@@ -1,4 +1,4 @@
-movies = JSON.parse(localStorage.getItem("movies")) || [];
+// movies = JSON.parse(localStorage.getItem("movies")) || [];
 
 var infoPelicula = document.querySelector("#info")
 
@@ -7,4 +7,13 @@ let detalle = JSON.parse(localStorage.getItem("detalle"))
 console.log("acaa:",detalle);
 
 infoPelicula.innerHTML = `
-<h4> ${detalle.descripcion}</h4>`
+
+<div class="card text-center h-100 w-50">            
+        <img src="${detalle.imgPortada}" class="card-img-top imgCard" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${detalle.nombrePelicula}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${detalle.descripcion}</h6>                          
+        </div>
+</div>
+
+`
