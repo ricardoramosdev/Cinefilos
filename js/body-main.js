4bc7d2da473954499cc439fe2cb5b5c6cbaec66c
Ricardo Ramos <indricardoramos@gmail.com>
2022-02-13 17:18:12 -0300

    tamaño cards index

--- js/body-main.js
+++ js/body-main.js
@@ -15,49 +15,37 @@ function categorias() {
         if(el.publicado){ //agrego esta condicion para que solo muestre las peliculas que el administrador tiene marcadas como publicadas
         if (el.categoria === "Drama") {
             Drama.innerHTML += `
-        <button class="btn my-3 boton col-6 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})"  >
+        <button class="btn my-1 boton col-4 p-1 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})"  >
             <div class="card paginas h-100 w-100 ">              
                     <img src="${el.imgPortada}" class=" imagenesCard " alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${el.nombrePelicula}</h5> 
-                        <h6 class="card-subtitle mb-2 text-muted">Mas populares</h6>                                                
-                    </div>                 
+                                 
             </div>     
         </button>          
         `
         } else if (el.categoria === "Acción") {
             Accion.innerHTML += `
-        <button  class="btn my-3 boton col-6 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})" >
+        <button  class="btn my-1 boton col-4 p-1 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})" >
             <div class="card paginas h-100 w-100 ">            
                         <img src="${el.imgPortada}" class=" imagenesCard  " alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.nombrePelicula}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Mas populares</h6>                          
-                        </div>
+                        
             </div>
         </button>
         `
         } else if (el.categoria === "Ciencia Ficción") {
             CiFi.innerHTML += `
-        <button class="btn my-3 boton col-6 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})" >
+        <button class="btn my-1 boton col-4 p-1 col-md-3 justify-content-center d-flex" onclick="detallePelicula(${index})" >
             <div class="card paginas h-100 w-100">
                         <img src="${el.imgPortada}" class=" imagenesCard" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title ">${el.nombrePelicula}</h5>    
-                            <h6 class="card-subtitle mb-2 text-muted">Mas populares</h6>     
-                        </div>
+                       
             </div>
         </button>
         `
         } else if (el.categoria === "Comedia") {
             Comedia.innerHTML += `
-        <button class="btn my-3 boton col-6 col-md-3 justify-content-center d-flex"  onclick="detallePelicula(${index})" >
+        <button class="btn my-1 boton col-4 p-1 col-md-3 justify-content-center d-flex"  onclick="detallePelicula(${index})" >
             <div class="card paginas h-100 w-100">
                         <img src="${el.imgPortada}" class=" imagenesCard" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.nombrePelicula}</h5>             
-                            <h6 class="card-subtitle mb-2 text-muted">Mas populares</h6>         
-                        </div>
+                        
             </div>
         </button>
         `
